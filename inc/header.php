b3f6bb99400ddb16f69acff13def0caecda271ce
Rajnish Patel <44401319+rajnish17100@users.noreply.github.com>
2019-03-25 03:21:25 +0530

    Create header.php

---
+++ inc/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+  <html>
+        <head>
+            <title>Credit Management</title>
+            <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.1.3/cerulean/bootstrap.min.css" rel="stylesheet" integrity="sha384-qVp3sGZJcZdk20BIG6O0Sb0sYRyedif3+Z8bZtQueBW/g7Dp67a0XdiMmmWCCm82" crossorigin="anonymous">
+           </head>
+    <body>
+    <?php include('navbar.php'); ?>
